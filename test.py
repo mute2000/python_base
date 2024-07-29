4c01265fc37c88ac03eb8f9e1f39da08cb58b1bc
Mute <1515587869@qq.com>
2024-07-29 12:06:35 +0800

    修改了test.py

--- test.py
+++ test.py
@@ -1,2 +1,28 @@
-# 查找列表中的最大值
-list = [1, 2, 3, 4, 5, 6]
+# 1.查找列表中的最大值
+# 方法一：max函数
+list = [4, 6, 8, 9, 2, 5, 1, 3]
+max = max(list)
+print(max)
+# 方法二：for循环
+max = 0
+for i in list:
+    if i > max:
+        max = i
+print(max)
+
+# 2.将列表从小到大排序
+# 1.sorted()函数
+sort = sorted(list)
+print(sort)
+# 2.冒泡排序
+for i in range(len(list)):
+    for j in range(len(list) - 1 - i):
+        if list[j] > list[j + 1]:
+            list[j], list[j + 1] = list[j + 1], list[j]
+print(list)
+
+# 逆转一个字符串
+str = "hello"
+# 切片
+new_str = str[::-1]
+print(new_str)
